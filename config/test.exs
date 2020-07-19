0409678ca974e82236910ff7fe152a8011539d17
Jonathan Yeong <jonathanyeong@gmail.com>
2020-07-19 13:32:46 -0700

    Bring back the GITHUB ACTIONS get_env in test.exs

--- config/test.exs
+++ config/test.exs
@@ -16,11 +16,11 @@ config :phoenix_blog, PhoenixBlog.Repo,
   pool: Ecto.Adapters.SQL.Sandbox
 
 # Configure the database for GitHub Actions
-# if System.get_env("GITHUB_ACTIONS") do
-#   config :phoenix_blog, PhoenixBlog.Repo,
-#     username: "postgres",
-#     password: "postgres"
-# end
+if System.get_env("GITHUB_ACTIONS") do
+  config :phoenix_blog, PhoenixBlog.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :phoenix_blog, PhoenixBlogWeb.Endpoint,
